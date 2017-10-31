00c6c96fa3c36bf6c2c2cbdb3f15adee006f4f4b
Rupsha <rupsha.bagchi@gmail.com>
2017-10-31 13:03:25 +0200

    video suggestions

---
+++ src/Components/videoitem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const VideoItem = ({video}) => {
+
+  return(
+    <li>{video.kind}</li>
+  )
+}
+
+export default VideoItem;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './Components/searchbar';
+import VideoList from './Components/videolist'
+import YTSearch from 'youtube-api-search'
+const API_KEY = 'AIzaSyA6Iv0ASORpI5vwLrE8t2llJCGbqZaeEDM';
 
-const API_KEY = 'AIzaSyCWNTi7_vAWeS1bFmGxb7j4mfsjp7jz2cU';
-const App = () => {
-   return (<div>
-            <SearchBar />
-          </div>
-  );
+class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        videos: []
+      }
+
+      YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+        this.setState({videos});
+      });
+    }
+
+    render(){
+      return (<div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+              </div>);
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
