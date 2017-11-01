c7eaf78850479ff74b07314a746a571bc6f10aa9
Rupsha <rupsha.bagchi@gmail.com>
2017-11-01 14:41:43 +0200

    styling

--- src/Components/video_plugin.js
+++ src/Components/video_plugin.js
@@ -7,7 +7,7 @@ const VideoPlugin = ({video}) => {
   const videoId = video.id.videoId;
   const url = `http://www.youtube.com/embed/${videoId}`;
   return(
-    <div className="video-plugin col-md-8">
+    <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
           <iframe class="embed-responsive-item" src={url} allowfullscreen></iframe>
       </div>
